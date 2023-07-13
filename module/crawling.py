3b7d1c7f556e27319792df3535e5614eee04d35d
Bill <bill.hanskim@gmail.com>
2023-07-13 23:41:18 +0900

    처음

--- module/crawling.py
+++ module/crawling.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_argument('--headless')        # Head-less 설정
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_experimental_option("detach", True)
 chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
